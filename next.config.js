db4c16e791e46b8b8007c63c50d72fd26d8890f2
Mayall0513 <mayall0513@outlook.com>
2024-07-05 09:34:17 +0100

    Standardize and address warnings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    FRONTEND_URI: process.env.FRONTEND_URI,
-    BACKEND_URI: process.env.BACKEND_URI,
-    DISCORD_API_URI: process.env.DISCORD_API_URI,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/avatars/**'
+      }
+    ]
   }
 }
